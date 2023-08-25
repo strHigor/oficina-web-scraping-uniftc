cc6c09fcc03080f6a0fad463fca514a562240ae9
Higor <higor.b97@hotmail.com>
2023-08-25 20:34:11 -0300

    first commit

---
+++ oficina_web_scraping.py
@@ -0,0 +1,31 @@
+import os
+import re
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+
+sessao = requests.session()
+anuncios_obtidos = []
+
+def iniciar_scraper():
+    procurar()
+    if anuncios_disponiveis:
+        extrair()
+
+
+def procurar():
+    pass
+
+
+def extrair():
+    pass
+                        
+                
+def corrigir_valor_monetario(valor_original):
+    valor_original = valor_original.replace('Â', '').replace('\xa0', ' ')
+    valor_corrigido = valor_original.encode('iso-8859-1').decode('utf-8')
+    return valor_corrigido.replace("R$ ", "").replace(".", "").replace(",", ".")
+
+
+iniciar_scraper()
